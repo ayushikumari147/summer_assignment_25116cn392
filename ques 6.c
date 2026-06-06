5f5dd394c730e739be897ad94ad3d15ed15d45e2
ayushikumari147 <ayushikumari147@gmail.com>
2026-06-06 21:41:01 +0530

    day 2 assignment

---
+++ ques 6.c
@@ -0,0 +1,17 @@
+/*write a program to reverse a number*/
+#include <stdio.h>
+int main(){
+    int n,digit;
+    printf("enter number:");
+    scanf("%d",&n);
+    int rev=0;
+    
+    while(n>0){
+        digit=n%10;
+        rev=rev*10+digit;
+        n=n/10;
+    }
+printf("the reverse of number =%d",rev);
+
+return 0;
+}
