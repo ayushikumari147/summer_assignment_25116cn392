0eea84f6b07a4ccf83d754beab7d77ded7c78c71
ayushikumari147 <ayushikumari147@gmail.com>
2026-06-11 00:22:58 +0530

    fox day 2 folder

---
+++ day 2/ques 5.c
@@ -0,0 +1,16 @@
+//write a program to find sum of digit of a number
+
+#include <stdio.h>
+int main(){
+    int n,digit;
+    printf("enter number:");
+    scanf("%d",&n);
+    int sum=0;
+    while(n>0){
+       digit=n%10;
+       sum=sum+digit;
+     n=n/10;
+    }
+    printf("the sum of digit =%d",sum);
+    return 0;
+}
